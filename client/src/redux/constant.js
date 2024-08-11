9da4a2a1405e42233920313307d72b379c1c3970
Naveen Vinod Kumar <157789096+Naveenvk29@users.noreply.github.com>
2024-08-12 03:29:11 +0530

    Update constant.js

--- client/src/redux/constant.js
+++ client/src/redux/constant.js
@@ -1,5 +1,5 @@
 export const BASE_URL = "";
-export const USER_URL = "/api/users";
-export const POST_URL = "/api/blogs";
-export const COMMENT_URL = "/api/comments";
-export const MESSAGES_URL = "/api/messages";
+export const USER_URL = "https://my-blog-api-ifwc.onrender.com/api/users";
+export const POST_URL = "https://my-blog-api-ifwc.onrender.com/api/blogs";
+export const COMMENT_URL = "https://my-blog-api-ifwc.onrender.com/api/comments";
+export const MESSAGES_URL = "https://my-blog-api-ifwc.onrender.com/api/messages";
